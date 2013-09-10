30e3ba8d8444f8707717b8f43638f76157adfd47
Morgan Jones <morgan@morganjones.org>
2013-09-10 13:53:09 -0400

    generalized, added config checking and command line arguments

--- update_db_frm_ldap.pl
+++ update_db_frm_ldap.pl
@@ -4,38 +4,63 @@
 use strict;
 use DBI;
 use Net::LDAP;
+use Getopt::Std;
 
 our %config;
 
-$ENV{ORACLE_BASE} = "/home/oracle";
-$ENV{ORACLE_HOME} = "/home/oracle/app/oracle/product/11.2.0.1/client_1";
+my %opts;
+getopts('nc:', \%opts);
 
+exists $opts{c} || print_usage();
 
-my $dbh = DBI->connect("dbi:Oracle:host=10.0.103.14;port=1567;sid=PROD1", "SLDAP", "pass") or
+print "-n used, ldap will not be modifed.\n"
+  if (exists $opts{n});
+
+require $opts{c};
+
+$ENV{ORACLE_BASE} = $config{oracle_base};
+$ENV{ORACLE_HOME} = $config{oracle_home};
+
+my $dbh = DBI->connect($config{connect_str}, $config{user}, $config{pass}) or
   die $DBI::errstr;
 
-my $sth = $dbh->prepare("TRUNCATE TABLE LDAP_STUDENTS");
-$sth->execute();
-exit;
+if ($config{truncate_table} =~ /yes/i) {
+    my $sth = $dbh->prepare($config{truncate_stmt});
+    $sth->execute();
+}
 
-my $ldap=Net::LDAP->new('ldaps://sgldap.philasd.net');
-my $bind_rslt = $ldap->bind("cn=directory manager", password => "CiDdti");
-$bind_rslt->code && die "unable to bind";
+my $ldap=Net::LDAP->new($config{ldap_host});
+my $bind_rslt = $ldap->bind($config{ldap_binddn}, password => $config{ldap_pass});
+$bind_rslt->code && die "unable to bind as $config{ldap_binddn}";
 
-my $rslt = $ldap->search(base=>"dc=philasd,dc=org", filter=>"(&(objectClass=sdpStudent)(sdpPrivateEmail=*)(!(memberOf=cn=sdpExcludes,ou=groups,dc=philasd,dc=org)))", attrs => ['sdpPrivateEmail', 'sdpSIDN']);
+my $rslt = $ldap->search(base=>$config{ldap_base}, filter=>$config{ldap_filter}, attrs => [@{$config{ldap_attrs}}]);
 $rslt->code && die "problem searching: ", $rslt->error;
 
-my $insert_sth = $dbh->prepare("INSERT INTO LDAP_STUDENTS (SIDN, EMAIL_ACCOUNT_ID) VALUES (?, ?)");
+my $insert_sth = $dbh->prepare($config{insert_stmt});
 
 for my $entry ($rslt->entries) {
-    my $email = ($entry->get_value('sdpprivateemail'))[0];
-    my $sidn = ($entry->get_value('sdpsidn'))[0];
-    next 
-      if ($email =~ /^\s*$/);
-    print "/$sidn/ /$email/\n";
-    $insert_sth->execute($sidn, $email);
+    # my $email = ($entry->get_value('sdpprivateemail'))[0];
+    # my $sidn = ($entry->get_value('sdpsidn'))[0];
+    my $next = 0;
+    my @values;
+    for my $attr (@{$config{ldap_attrs}}) {
+	push @values, ($entry->get_value($attr))[0];
+	$next = 1
+	  if (($entry->get_value($attr))[0] =~ /^\s*$/ && $config{skip_if_attrs_empty} =~ /yes/i);
+    }
+    next
+      if ($next);
+    print join (' ', @values), "\n";
+    
+    $insert_sth->execute(@values);
 }
 
 #$dbh->commit;
 $dbh->disconnect;
 
+
+    sub print_usage {
+	print "usage: $0 [-n] -c config.cf\n\n";
+	exit;
+    }
+	
